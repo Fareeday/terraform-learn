1e647cbf821e1af795b00564b542a450851e2635
Fareed <fareed_farooq@hotmail.com>
2024-08-27 16:56:15 +0500

    Deplyed EC2 Using Terrform

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -20,14 +20,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_vpc" "terra_vpc" {
+/*data "aws_vpc" "terra_vpc" {
   id = "vpc-084b71d52d7e2e46c" 
+}*/
+
+resource  "aws_vpc"  "terra_vpc"  {
+  cidr_block  = "10.0.0.0/16"
+
+  tags  = {
+    Name  = "aws_vpc.terra_vpc"
+  }
+}
+
+resource "aws_subnet" "terra_pub_subnet" {
+  vpc_id  = "vpc-03afe8a83fd32b67b"
+  cidr_block  = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags  = {
+    Name  = "terra_pub_subnet"
+  }
+}
+
+resource  "aws_network_interface"  "terra_if"  {
+  subnet_id = "subnet-0d8a354ff11e23954"
+  private_ips = ["10.0.1.10"]
 }
 
 resource "aws_security_group" "allow_web_ssh" {
   name        = "MyServer SG Group"
   description = "MyServer Security Group"
-  vpc_id      = data.aws_vpc.terra_vpc.id
+  vpc_id      = "vpc-03afe8a83fd32b67b"
 
   ingress =  [
   { description       = "SSH"
@@ -77,6 +100,7 @@ resource "aws_instance" "my_server" {
   ami = "ami-02c21308fed24a8ab"
   instance_type = "t2.micro"
   key_name  = "${aws_key_pair.deployer.key_name}"
+  subnet_id = "subnet-0d8a354ff11e23954"
   vpc_security_group_ids  = [aws_security_group.allow_web_ssh.id]
   user_data = data.template_file.user_data.rendered
   tags = {
@@ -88,6 +112,6 @@ output  "public_ip"  {
   value = aws_instance.my_server.public_ip
 }
 
-/*output  "sg_id"  {
+output  "sg_id"  {
   value = aws_security_group.allow_web_ssh.id
-}*/
+}
