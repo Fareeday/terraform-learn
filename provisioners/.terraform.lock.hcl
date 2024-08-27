642db473c9798255a2a7dd64d663ceaf7d3e5369
Fareed <fareed_farooq@hotmail.com>
2024-08-27 14:06:58 +0500

    Added Provisioners

---
+++ provisioners/.terraform.lock.hcl
@@ -0,0 +1,42 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.64.0"
+  constraints = "5.64.0"
+  hashes = [
+    "h1:Xasb457vfMG/1SGu6KSApCzAqUHMlsL028OQu3dZVv8=",
+    "zh:1d361f8062c68c9d5ac14b0aa8390709542129b8a9b258e61bbbabc706078b44",
+    "zh:39dcbf53e3896bdd77071384c8fad4a5862c222c73f3bcf356aca488101f22fd",
+    "zh:3fad63505f0c5b6f01cc9a6ef02b2226983b79424126a9caf6eb724f654299f4",
+    "zh:53a8b90d00829cc27e3171a13a8ff1404ee0ea018e73f31d3f916d246cc39613",
+    "zh:5734c25ef5a04b40f3c1ac5f817f11e42ee3328f74dbc141c0e64afbb0acc834",
+    "zh:66ea14dbd87f291ce4a877123363933d3ca4022f209f885807a6689c22c24e80",
+    "zh:68e79654ad0894a3d93134c3377748ace3058d5fad5ec09d1e9a8f8f9b8a47ea",
+    "zh:7b74259d0ceef0c49cea6bcd171df997b6bad141085bbadded15b440faeb0eee",
+    "zh:988ebfb5d115dc57070b5abf2e4200ad49cde535f27fd2ba5e34cf9ab336a57f",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:a0a2d4efe2835f0101a0a5024e044a3f28c00e10a8d87fce89c707ef6db75cea",
+    "zh:aecb3e4b9121771dee9cac7975bf5d0657b5f3e8b57788c455beaeb0f3c48d93",
+    "zh:d2d3393170b8ef761d3146f39f6788c4a3e876e6c5d4cedca4870c2680688ae6",
+    "zh:daba5a005c1baa4a5eefbfb86d43ccf880eb5b42e8136f0d932f55886d72bda0",
+    "zh:de16a6ff3baacdaf9609a0a89aa1913fc19cccaf5ee0fc1c49c5a075baa47c02",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/template" {
+  version = "2.2.0"
+  hashes = [
+    "h1:94qn780bi1qjrbC3uQtjJh3Wkfwd5+tTtJHOb7KTg9w=",
+    "zh:01702196f0a0492ec07917db7aaa595843d8f171dc195f4c988d2ffca2a06386",
+    "zh:09aae3da826ba3d7df69efeb25d146a1de0d03e951d35019a0f80e4f58c89b53",
+    "zh:09ba83c0625b6fe0a954da6fbd0c355ac0b7f07f86c91a2a97849140fea49603",
+    "zh:0e3a6c8e16f17f19010accd0844187d524580d9fdb0731f675ffcf4afba03d16",
+    "zh:45f2c594b6f2f34ea663704cc72048b212fe7d16fb4cfd959365fa997228a776",
+    "zh:77ea3e5a0446784d77114b5e851c970a3dde1e08fa6de38210b8385d7605d451",
+    "zh:8a154388f3708e3df5a69122a23bdfaf760a523788a5081976b3d5616f7d30ae",
+    "zh:992843002f2db5a11e626b3fc23dc0c87ad3729b3b3cff08e32ffb3df97edbde",
+    "zh:ad906f4cebd3ec5e43d5cd6dc8f4c5c9cc3b33d2243c89c5fc18f97f7277b51d",
+    "zh:c979425ddb256511137ecd093e23283234da0154b7fa8b21c2687182d9aea8b2",
+  ]
+}
---
+++ provisioners/main.tf
@@ -0,0 +1,93 @@
+terraform { 
+  /*cloud { 
+    
+    organization = "Fareedev" 
+
+    workspaces { 
+      name = "provisioners" 
+    } 
+  } */
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "terra_vpc" {
+  id = "vpc-084b71d52d7e2e46c" 
+}
+
+resource "aws_security_group" "allow_web_ssh" {
+  name        = "MyServer SG Group"
+  description = "MyServer Security Group"
+  vpc_id      = data.aws_vpc.terra_vpc.id
+
+  ingress =  [
+  { description       = "SSH"
+    from_port         = "22"
+    to_port           = "22"
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = []
+    prefix_list_ids   = []
+    security_groups   = []
+    self              = false
+  },
+  { description       = "HTTP"
+    from_port         = "80"
+    to_port           = "80"
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = []
+    prefix_list_ids   = []
+    security_groups   = []
+    self              = false
+  }
+]
+  egress  = [
+  { description       = "Outbound Traffic"
+    from_port         = 0
+    to_port           = 0
+    protocol           = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = ["::/0"]
+    prefix_list_ids   = []
+    security_groups   = []
+    self              = false
+  }
+  ]
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1j3mUUVOvDLyPC0kpC5grfNjjwRuZqIKfwIkr/aU730vjaiZlsB92PQRIPqWBbvQp9Q3tEgGK9m9BM7pmf7l1oYu4bsd/G1xuxdpQan8axKS/2MxnDFML3TE+bIyOkcjE4d3ESptZzUo448g15ACE4svQ44YzQdr99p5tLGFFDFKOhXhe6d7bFYy4R8Kqajbemf+htEQtoY908lU3HjEwcv8DRwMv/tmU2zqoJ7ck2ZvIcfXjKXPP9koxJJ9b4CqPLIAkvk78bzmnwAR6HjkII5VwIWcpQmUxJU3+JD1iFfKgkIpPzH3ER51MMJK5zxucXWUCePV56AhCIsi2TExCY32Wh3dKcYaWAO2xxFtOk5tOfJLUebN7DNJPCoYf5MHW+6Is/knNl4T6e2h3etGT7CSbXlAPqxLnvIHW63427Gro+Qx2bFTQ8GXCr2FFw6smYWXtPaaMYIHNzAjcW7qt/9iUQXO066OxpteyZm8B0E6OMtz5GlTlEC2pNrPQYPs= root@fareed-Devops"
+}
+
+data  "template_file"  "user_data"  {
+    template = file("./userdata.yaml")
+}
+resource "aws_instance" "my_server" {
+  ami = "ami-02c21308fed24a8ab"
+  instance_type = "t2.micro"
+  key_name  = "${aws_key_pair.deployer.key_name}"
+  vpc_security_group_ids  = [aws_security_group.allow_web_ssh.id]
+  user_data = data.template_file.user_data.rendered
+  tags = {
+    Name  = "MyServer"
+  }
+}
+
+output  "public_ip"  {
+  value = aws_instance.my_server.public_ip
+}
+
+/*output  "sg_id"  {
+  value = aws_security_group.allow_web_ssh.id
+}*/
